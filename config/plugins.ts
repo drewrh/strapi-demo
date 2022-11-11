a71a592c875593d0808863764a11be2b7a191a90
drewrh <drewrh04@gmail.com>
2022-11-11 17:41:43 -0500

    Initial commit

---
+++ config/plugins.ts
@@ -0,0 +1,9 @@
+import crypto from 'crypto';
+
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+        jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64')
+    },
+  },
+});
